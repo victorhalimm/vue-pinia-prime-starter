63b20fadd0de1ddd1f2012c53f8392058178c2b8
victorhalimm <victorhalim18@gmail.com>
2025-05-16 16:51:57 +0700

    todo: make sure pinia and primevue works

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-black w-full min-h-screen">
+    <h1 class="text-blue-200 text-4xl">Vue Starter</h1>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  plugins: [vue(), tailwindcss()],
+})
